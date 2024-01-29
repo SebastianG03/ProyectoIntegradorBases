bfa53cb9d6c6964b8c972b947a7dadffc79984e7
Christian Jacome <christian.jacome.mora@udla.edu.ec>
2024-01-29 09:42:08 -0500

    Se agregan las reestricciones de la tabla de desalojo

--- init-reporte.sql
+++ init-reporte.sql
@@ -1,9 +1,15 @@
 CREATE TABLE dbo.Desalojo(
-    IdHuesped INT,
-    Motivo VARCHAR(50),
-    FechaDeDesalojo DATETIME,
-
+    Id INT IDENTITY,
+    -- La relación entre HUESPED-DESALOJO no debe ser NULL
+    IdHuesped INT NOT NULL,
+    -- El motivo del desalojo no puede ser NULL
+    Motivo VARCHAR(50) NOT NULL,
+    FechaDeDesalojo DATETIME
+    -- REESTRICCIONES
+        -- Llave Principal
+    CONSTRAINT PK_Desalojo PRIMARY KEY(Id)
+        -- El motivo del desalojo no puede estar vacío
+    CONSTRAINT CK_MotivoDesalojo CHECK (Motivo != '')
     -- Llaves Foráneas
-    FOREIGN KEY (IdHuesped) 
-    REFERENCES dbo.Huespedes(Id)
+    FOREIGN KEY (IdHuesped) REFERENCES dbo.Huespedes(Id)
 );
